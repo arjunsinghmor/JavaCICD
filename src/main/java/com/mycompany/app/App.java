561ec5bab8ab4c8308a94ff93b50a0caeb2909b5
arjunsinghmor <arjunsinghmor@gmail.com>
2022-09-19 22:15:27 +0530

    Added files for CICD

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    private final String message = "Welcome to Vitraya Tech!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+		
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
